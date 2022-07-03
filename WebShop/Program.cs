c45030fbe7546a3039be77bffd03abde599e7d8d
AmarHodzic <43781481+AmarHodzic@users.noreply.github.com>
2022-07-04 00:46:52 +0200

    final

--- WebShop/Program.cs
+++ WebShop/Program.cs
@@ -47,8 +47,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: myAllowSpecificOrigins,
         builder =>
         {
-            //builder.WithOrigins("http://mojwebshops.com")
-            builder.WithOrigins("http://localhost:4200")
+            builder.WithOrigins("http://mojwebshops.com")
+            //builder.WithOrigins("http://localhost:4200")
             .AllowAnyMethod()
             .AllowAnyHeader();
         });
